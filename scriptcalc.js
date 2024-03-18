5f3932884688a4b4a7da4388ce8e3cd45ecc5780
Hiroyuki-Iijima-RC <158529895+Hiroyuki-Iijima-RC@users.noreply.github.com>
2024-03-18 23:03:06 +0900

    Update scriptcalc.js

--- scriptcalc.js
+++ scriptcalc.js
@@ -1,16 +1,14 @@
 'use strict'
 // 1行目に記載している 'use strict' は削除しないでください
 
-let randomNunber, x, arith, y, answer;
+let randomNunber, x, arith, y, answer, tempNumber;
 let time = 3;
 let question = 0;
 let questionObject = [];
 let countDownTimer;
 let implement = false;
 
-const body = document.body;
-const container = document.getElementsByClassName("container");
-
+// 画面表示コメントの要素取得
 const comment = document.getElementById("comment");
 const numberX = document.getElementById("numberX");
 const arithmetic = document.getElementById("arithmetic");
@@ -18,16 +16,17 @@ const numberY = document.getElementById("numberY");
 const timemessage = document.getElementById("timemessage");
 const timercount = document.getElementById("timercount");
 
+// 3択ボタンの要素取得
 const choice1 = document.getElementById("choice1");
 const choice2 = document.getElementById("choice2");
 const choice3 = document.getElementById("choice3");
 
+// スタート・リトライ・戻るボタンの要素取得
 const startButton = document.getElementById("startButton");
 const retryButton = document.getElementById("retryButton");
 const backButton = document.getElementById("backButton");
-// const plusButton = document.getElementById("plusButton");
-// const minusButton = document.getElementById("minusButton");
-// const resetButton = document.getElementById("resetButton");
+
+const table = document.getElementById("table");
 
 
 startButton.addEventListener("click", start);
@@ -35,7 +34,7 @@ choice1.addEventListener("click", calcJudge1);
 choice2.addEventListener("click", calcJudge2);
 choice3.addEventListener("click", calcJudge3);
   
-
+// スタートボタンが押されたときに実行
 function start() {
   // console.log(time);
   comment.innerText = "";
@@ -46,8 +45,9 @@ function start() {
   countDownTimer = setInterval(countDown, 1000);
 }
 
+// 3カウント→10カウント処理
 function countDown() {
-  if (time === 1 && implement === false) {
+  if (time === 1 && implement === false) {  // 3カウントダウン後、テスト開始時に動く
     // clearInterval(countDown);
 
     implement = true;
@@ -62,7 +62,7 @@ function countDown() {
     timercount.innerText = time;
     timercount.style.visibility = "visible";
     decideArithmetic();
-  } else if (time === 1 && implement === true) {
+  } else if (time === 1 && implement === true) {  // テスト終了時に動く
     time = 3;
     implement = false;
     clearInterval(countDownTimer);
@@ -78,18 +78,16 @@ function countDown() {
     choice3.style.visibility = "hidden";    
     retryButton.style.visibility = "visible";
     backButton.style.visibility = "visible";
-  } else if (implement === false) {  
+  } else if (implement === false) {   // 最初の3カウントダウン
     time -= 1;
-    // console.log(time);
     arithmetic.innerText = time;
-  } else if (implement === true) {
+  } else if (implement === true) {    // 10カウントダウン
     time -= 1;
-    // console.log(time);
     timercount.innerText = time;
   }
 }
 
-
+// 計算式作成
 function decideArithmetic() {
   const decideResult = [];
 
@@ -105,12 +103,19 @@ function decideArithmetic() {
     arith = "÷";
   }
 
-  y = Math.floor(Math.random() * 10 + 1);
+  y = Math.floor(Math.random() * 9 + 1);
 
   if (arith === "÷") {
-    x = y * Math.floor(Math.random() * 10 + 1);
+    x = y * Math.floor(Math.random() * 9 + 1);
   } else {
-    x = Math.floor(Math.random() * 10 + 1);
+    x = Math.floor(Math.random() * 9 + 1);
+  }
+
+  // 引き算の時に計算結果がマイナスにならないようにする
+  if (x < y && arith === "-") {
+    tempNumber = y;
+    y = x;
+    x = tempNumber;
   }
 
   if (arith === "+") {
@@ -157,19 +162,12 @@ function decideArithmetic() {
     decideResult.push("3");  
   }
 
-  // console.log(decideResult);
-
 
   numberX.innerText = x;
   arithmetic.innerText = arith;
   numberY.innerText = y;
-  // console.log(question);
-  // choice1.innerText = answer - 1;
-  // choice2.innerText = answer;
-  // choice3.innerText = answer + 1;
-  // questionObject =
   questionObject.push(decideResult);
-  // console.log(questionObject);
+
 
   return decideResult;
 }
@@ -177,11 +175,27 @@ function decideArithmetic() {
 function calcJudge1() {
   if (questionObject[question][7] === "1") {
     questionObject[question].push("○");
-    // console.log("疎通");
   } else {
     questionObject[question].push("×");
   }
-  // console.log(questionObject[question]);
+
+  let tr = document.createElement("tr");
+
+  let td1 = document.createElement("td");
+  td1.innerText = questionObject[question][1] + questionObject[question][2] + questionObject[question][3];
+  console.log(td1);
+  tr.appendChild(td1);
+
+  let td2 = document.createElement("td");
+  td2.innerText = questionObject[question][4];
+  tr.appendChild(td2);
+
+  let td3 = document.createElement("td");
+  td3.innerText = questionObject[question][8];
+  tr.appendChild(td3);  
+
+  table.appendChild(tr);
+
   question++;
   decideArithmetic();
 }
@@ -189,11 +203,27 @@ function calcJudge1() {
 function calcJudge2() {
   if (questionObject[question][7] === "2") {
     questionObject[question].push("○");
-    // console.log("疎通");
   } else {
     questionObject[question].push("×");
   }
-  // console.log(questionObject[question]);
+  
+  let tr = document.createElement("tr");
+
+  let td1 = document.createElement("td");
+  td1.innerText = questionObject[question][1] + questionObject[question][2] + questionObject[question][3];
+  console.log(td1);
+  tr.appendChild(td1);
+
+  let td2 = document.createElement("td");
+  td2.innerText = questionObject[question][5];
+  tr.appendChild(td2);
+
+  let td3 = document.createElement("td");
+  td3.innerText = questionObject[question][8];
+  tr.appendChild(td3);  
+
+  table.appendChild(tr);
+
   question++;
   decideArithmetic();
 }
@@ -201,203 +231,27 @@ function calcJudge2() {
 function calcJudge3() {
   if (questionObject[question][7] === "3") {
     questionObject[question].push("○");
-    // console.log("疎通");
   } else {
     questionObject[question].push("×");
   }
-  // console.log(questionObject[question]);
+  
+  let tr = document.createElement("tr");
+
+  let td1 = document.createElement("td");
+  td1.innerText = questionObject[question][1] + questionObject[question][2] + questionObject[question][3];
+  console.log(td1);
+  tr.appendChild(td1);
+
+  let td2 = document.createElement("td");
+  td2.innerText = questionObject[question][6];
+  tr.appendChild(td2);
+
+  let td3 = document.createElement("td");
+  td3.innerText = questionObject[question][8];
+  tr.appendChild(td3);  
+
+  table.appendChild(tr);
+
   question++;
   decideArithmetic();
 }
-
-
-
-
-// function plusCount() {
-//   count++;
-
-//   if (judgeThree(count)[0] !== 0) {
-//     counter100.innerHTML = judgeThree(count)[0];
-//   }
-//   if (judgeThree(count)[1] !== 0 || judgeThree(count)[0] !== 0) {
-//     counter10.innerHTML = judgeThree(count)[1];
-//   }
-//   counter1.innerHTML = judgeThree(count)[2];
-
-//   if (judgeThree(count)[3]) {
-//     counter100.style.color = "Red";
-//   } else {
-//     counter100.style.color = "";    
-//   }
-  
-//   if (judgeThree(count)[4]) {
-//     counter10.style.color = "Red";
-//   } else {
-//     counter10.style.color = "";    
-//   }
-
-//   if (judgeThree(count)[5]) {
-//     counter1.style.color = "Red";
-//   } else {
-//     counter1.style.color = "";    
-//   }
-
-//   if (judgeThree(count)[6]) {
-//     body.style.backgroundColor = "aqua";
-//   } else {
-//     body.style.backgroundColor = "";
-//   }
-
-// }
-
-// function minusCount() {
-//   if (count > 0) { 
-//     count--;
-//   if (judgeThree(count)[0] !== 0) {
-//     counter100.innerHTML = judgeThree(count)[0];
-//   } else {
-//     counter100.innerHTML = "";
-//   }
-//   if (judgeThree(count)[1] !== 0 || judgeThree(count)[0] !== 0) {
-//     counter10.innerHTML = judgeThree(count)[1];
-//   } else {
-//     counter10.innerHTML = "";
-//   }
-//   counter1.innerHTML = judgeThree(count)[2];
-//   }
-
-
-//   if (judgeThree(count)[3]) {
-//     counter100.style.color = "Red";
-//   } else {
-//     counter100.style.color = "";    
-//   }
-  
-//   if (judgeThree(count)[4]) {
-//     counter10.style.color = "Red";
-//   } else {
-//     counter10.style.color = "";    
-//   }
-  
-//   if (judgeThree(count)[5]) {
-//     counter1.style.color = "Red";
-//   } else {
-//     counter1.style.color = "";    
-//   }
-
-//   if (judgeThree(count)[6]) {
-//     body.style.backgroundColor = "aqua";
-//   } else {
-//     body.style.backgroundColor = "";
-//   }
-
-// }
-
-// function resetCount() {
-//   count = 0;
-//   counter100.innerText = "";
-//   counter10.innerText = "";
-//   counter1.innerText = 0;
-
-//   counter100.style.color = "";
-//   counter10.style.color = "";
-//   counter1.style.color = "";
-//   body.style.backgroundColor = "";
-// }
-
-
-// function test(actual, expected) {
-//   if (JSON.stringify(actual) === JSON.stringify(expected)) {
-//     console.log("OK! Test PASSED.");
-//   } else {
-//     console.error("Test FAILED. Try again!");
-//     console.log("    actual: ", actual);
-//     console.log("  expected: ", expected);
-//     console.trace();
-//   }
-// }
-
-// ３の倍数と３が付く数字を判定する関数
-// function judgeThree(num) {
-//   const judgeArray = [0, 0, 0, false, false, false, false]; // [0,0,0,true,true,true,true][3桁目,2桁目,1桁目,3桁目が3,2桁目が3,1桁目が3,3の倍数か]
-//   // console.log(num.toString().length);
-
-//   if (num.toString().length === 1) {
-//     judgeArray[2] = (Number(num.toString()[0]));
-//     if (Number(num.toString()[0]) === 3) {
-//       judgeArray[5] = true;  
-//     } 
-//   } else if (num.toString().length === 2) {
-//     judgeArray[1] = (Number(num.toString()[0]));
-//     judgeArray[2] = (Number(num.toString()[1]));
-//     if (Number(num.toString()[0]) === 3) {
-//       judgeArray[4] = true;
-//     }
-//     if (Number(num.toString()[1]) === 3) {
-//       judgeArray[5] = true;
-//     }
-//   } else if (num.toString().length === 3) {
-//     judgeArray[0] = (Number(num.toString()[0]));
-//     judgeArray[1] = (Number(num.toString()[1]));
-//     judgeArray[2] = (Number(num.toString()[2]));
-//     if (Number(num.toString()[0]) === 3) {
-//       judgeArray[3] = true;
-//     }
-//     if (Number(num.toString()[1]) === 3) {
-//       judgeArray[4] = true;
-//     }
-//     if (Number(num.toString()[2]) === 3) {
-//       judgeArray[5] = true;
-//     }
-//   }
-
-//     // for (let i = 0; i < num.toString().length; i++) {
-//     //     // console.log("i");]
-//     //     // if (Number(num.toString()[i]) === 3) {
-//     //     //     judge = true;
-//     //     //     break;
-//     //     // }
-
-//     //     // console.log(Number(num.toString()[i]));
-//     //   if (i === 0 && num.toString().length === 1 || i === 1 && num.toString().length === 2) {
-//     //     judgeArray[2] = (Number(num.toString()[2]));
-//     //     if (Number(num.toString()[2]) === 3) {
-//     //       judgeArray[5] = true;
-//     //     }
-//     //   } else if (i === 0 && num.toString().length === 2 || i === 1 && num.toString().length === 3) {
-//     //     judgeArray[1] = (Number(num.toString()[1]));
-//     //     if (Number(num.toString()[1]) === 3) {
-//     //       judgeArray[4] = true;
-//     //     }
-//     //   } else {
-//     //     judgeArray[0] = (Number(num.toString()[0]));
-//     //     if (Number(num.toString()[0]) === 3) {
-//     //       judgeArray[3] = true;
-//     //     }
-//     //   }
-      
-//     //     // console.log(Number(num.toString()[i]) === 3);
-//     //     // console.log(Number(num.toString()[i]));
-//     // }
-    
-//   if (num % 3 === 0 && num !== 0) {
-//     judgeArray[6] = true;
-//   }
-//   console.log(judgeArray);
-//     // console.log(Number(num.toString()[0]) === 3);
-//   return judgeArray;
-// }
-
-
-
-// test(judgeThree(103),true);
-// test(judgeThree(31),true);
-// test(judgeThree(32),true);
-// test(judgeThree(33),true);
-// test(judgeThree(34),true);
-// test(judgeThree(35),true);
-// test(judgeThree(36),true);
-// test(judgeThree(37),true);
-// test(judgeThree(38),true);
-// test(judgeThree(39),true);
-// test(judgeThree(40),true);
